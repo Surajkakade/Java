5ee2c175f54eb76a4ce7999238397cd03d02e429
SURAJ KAKADE <79645053+Surajkakade@users.noreply.github.com>
2021-04-22 11:42:02 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,79 @@
+Class test{
+
+class Address {
+
+    String addressLine;
+
+    String city;
+
+    String zip;
+  
+  Address(String aline,String c,String z){
+    this.addressLine=aline;
+    this.city=c;
+    this.zip=z;
+  }
+  
+
+}
+
+ 
+class Person extends Address{
+ 
+
+ int id;
+ String name;
+  Address add;
+
+  Person(int id, String name,Address a){
+    this.id=id;
+    this.name=name;
+    this.add=a;
+  }
+
+}
+ 
+
+class NaturalPerson extends Person
+
+{
+
+    Date dateOfBirth;
+
+    int age;
+    Person per;
+
+NaturalPerson(Person p, Date d, int a){
+  this.per=p;
+  this.date=d;
+  this.age.a;
+ 
+ }
+    
+    
+
+}
+
+ 
+
+ 
+
+class LegalPerson extends Person
+
+{
+
+    int numberOfEmployees;
+    Person per;
+    LegalPerson(Person p, int n){
+    this.per=p;
+    this.numberOfEmployees=n;
+}
+
+public static void main(String args[])
+{
+ System.out.println("printing for no operations");
+}
+  
+  
+  
+} //test
